a3df932075bbca883e40309bcc2c20dc635d5392
nollpe <100038250+nollpe@users.noreply.github.com>
2022-03-27 23:05:18 +0200

    ribancos kifli

--- src/tester/testerClass.java
+++ src/tester/testerClass.java
@@ -36,7 +36,7 @@ public class testerClass {
     }
 
     //region norbi tesztek
-    public void wiewCity()
+    public void viewCity()
     {
         City c= new City();
         c.showCity();
@@ -318,7 +318,7 @@ public class testerClass {
     {
         testerClass ts=new testerClass();
         ts.agentExpires();
-        ts.wiewCity();
+        ts.viewCity();
         ts.startGame();
         ts.endTurn();
     }
