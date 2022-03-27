ad8f10213e08ddcee3ed8aac4f12e3a86c9ba263
nollpe <100038250+nollpe@users.noreply.github.com>
2022-03-27 14:52:36 +0200

    branchxd

--- src/agents/Chorea.java
+++ src/agents/Chorea.java
@@ -1,16 +1,17 @@
 package agents;
 
 import agents.Agent;
+import character.Player;
 import tester.testerClass;
 
 public class Chorea extends Agent
 {
-    public void takeEffect(Character c)
+    public void takeEffect(Player c)
     {
         testerClass.print();
     }
 
-    public void loseEffect(Character c)
+    public void loseEffect(Player c)
     {
         testerClass.print();
     }
