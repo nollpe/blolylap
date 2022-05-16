940fbf624c070d04938317847a9430ce2b9f1be0
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2022-05-16 11:05:27 +0200

    bag view

--- src/graphics/Equipments/BagControl.java
+++ src/graphics/Equipments/BagControl.java
@@ -4,8 +4,6 @@ import equipment.Equipment;
 
 import java.awt.*;
 
-public class BagControl extends  EquipmentView{
-    public BagControl(Rectangle r, String ImageName, Equipment e) {
-        super(r, "kepek/bag.png", e);
-    }
+public class BagControl{
+
 }
--- src/graphics/Equipments/BagView.java
+++ src/graphics/Equipments/BagView.java
@@ -1,4 +1,11 @@
 package graphics.Equipments;
 
-public class BagView {
+import equipment.Equipment;
+
+import java.awt.*;
+
+public class BagView  extends  EquipmentView {
+    public BagView(Rectangle r, String ImageName, Equipment e) {
+        super(r, "kepek/bag.png", e);
+    }
 }
