f786f4382b21f61a53aa7c676b017a34c0076639
zsohajdu1 <zsohajdu1@gmail.com>
2022-05-16 10:23:21 +0200

    meg akarom ölni magam

--- src/graphics/IView.java
+++ src/graphics/IView.java
@@ -5,13 +5,13 @@ import javax.swing.*;
 public interface IView
 {
     public JLabel label = new JLabel();
-
     public default JLabel getLabel()
     {
         return label;
     }
     public void Update();
     public void Event();
+
 
 
 
