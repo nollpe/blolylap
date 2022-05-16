dbb9b1f52a0674f4906bdcc6537f359fa9063515
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2022-05-16 10:49:42 +0200

    axe view

--- src/graphics/Equipments/AxeView.java
+++ src/graphics/Equipments/AxeView.java
@@ -1,4 +1,11 @@
 package graphics.Equipments;
 
-public class AxeView {
+import equipment.Equipment;
+
+import java.awt.*;
+
+public class AxeView extends EquipmentView{
+    public AxeView(Rectangle r, String ImageName, Equipment e) {
+        super(r, "kepek/axe.png", e);
+    }
 }
