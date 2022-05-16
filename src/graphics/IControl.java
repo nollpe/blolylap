57155d13c863cf51619cf1947da93983f54ae1e9
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2022-05-16 20:48:16 +0200

    iview handle event argumentum kiegeszites

--- src/graphics/IControl.java
+++ src/graphics/IControl.java
@@ -1,5 +1,7 @@
 package graphics;
 
+import java.awt.event.MouseEvent;
+
 public interface IControl {
-    public void handleEvent(/*event?*/);
+    public void handleEvent(MouseEvent e);
 }
--- src/graphics/agents/AgentControl.java
+++ src/graphics/agents/AgentControl.java
@@ -4,13 +4,15 @@ import agents.Agent;
 import character.Player;
 import graphics.IControl;
 
+import java.awt.event.MouseEvent;
+
 public class AgentControl implements IControl {
     Player ownerPlayer;
     Agent owner;
     AgentView view;
 
     @Override
-    public void handleEvent() {
+    public void handleEvent(MouseEvent e) {
 
     }
 }
