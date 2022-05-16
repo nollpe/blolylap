87a578a9c5e4c555f6e6cb9656d875970a9aecda
nollpe <100038250+nollpe@users.noreply.github.com>
2022-05-16 21:57:44 +0200

    fozosmcs

--- src/graphics/GraphicsConstroller.java
+++ src/graphics/GraphicsConstroller.java
@@ -5,7 +5,9 @@ import agents.Chorea;
 import character.Player;
 import equipment.Equipment;
 import equipment.Gloves;
+import equipment.Labcoat;
 import field.Field;
+import graphics.GeneticCode.GeneticCodeView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -63,7 +65,9 @@ public class GraphicsConstroller {
         panel = new JPanel();
 
         turnOf.addEquipment(new Gloves());
+        turnOf.addEquipment(new Labcoat());
         turnOf.addCastableAgent(new Chorea(turnOf));
+
         //háttér betöltése
         int sides = turnOf.getLocation().getNeighbours().size();
 
@@ -85,7 +89,7 @@ public class GraphicsConstroller {
 
         for (Equipment eq : turnOf.getStored()) {
             views.add(eq.getView());
-            eq.getView().getLabel().setBounds(200 + i * 80, 100, 60, 60);
+            eq.getView().getLabel().setBounds(20, 40+80*i, 60, 60);
             i++;
 
             //equimpemntLabels.add(eq.getView().getLabel());
@@ -93,17 +97,24 @@ public class GraphicsConstroller {
 
         //szomszédok tm
         LinkedList<Field> neighbours=turnOf.getLocation().getNeighbours();
-        neighbours.add(turnOf.getLocation());
+        i=0;
+        double theta = 2 * Math.PI / sides;
+
         for(Field f:neighbours)
         {
+            double x = Math.cos(theta * i-Math.PI/(sides*sides));
+            double y = Math.sin(theta * i-Math.PI/(sides*sides));
             views.add( f.getView());
-            f.getView().getLabel().setBounds(i*60,i*60,60,60);i++;
+            f.getView().getLabel().setBounds(500+(int)(250*x),300+(int)(200*y),60,60);i++;
         }
+
+        //főzősmcs
+        GeneticCodeView makeAgent=new GeneticCodeView(turnOf);
+        panel.add(makeAgent.getLabel());
 
         //inventory TODO
 
-        //főzős
-        JLabel fozos = new JLabel();
+
         //fozos.add(new MouseListener());
         // TODO kell mouslistener amire megjelenik egy combobox ami tartalmazza a csinálható ágenseket, ha rákattintasz megcsinálja
 
