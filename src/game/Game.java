7b583059f9c51d8fa6873e50164b785a72069708
nollpe <100038250+nollpe@users.noreply.github.com>
2022-05-17 12:24:25 +0200

    nagy csongor páljya

--- src/game/Game.java
+++ src/game/Game.java
@@ -368,8 +368,8 @@ public class Game {
         final int[] revalue = {0};
         JFrame manuframe=new JFrame();
         JComboBox combobox;
-        String geneticCodesString[] = new String[5];
-        for (int i = 0; i < 5; i++) {
+        String geneticCodesString[] = new String[7];
+        for (int i = 0; i < 7; i++) {
             geneticCodesString[i] = String.valueOf(i+1);
 
         }
@@ -409,11 +409,11 @@ public class Game {
 
     public void startGame() {
         int players=Menu();
-        city.generateMap();
+        city.generateMap(players);
 
         for(int i=0;i<players;i++)
         {
-            Player tempPlayer=new Player("player"+(i+1));
+            Player tempPlayer=new Player("player"+(i));
             allPlayers.add(tempPlayer);
         }
 
