6dcb476142699511d705999a43edc2d5cb279f69
radnotizsuzsanna <radnotizsuzsanna42@gmail.com>
2022-05-17 13:20:40 +0200

    fieldview kommentek

--- src/graphics/fields/FieldControl.java
+++ src/graphics/fields/FieldControl.java
@@ -4,16 +4,33 @@ import character.Player;
 import field.Field;
 import game.Game;
 
+/**
+ * A field kontrol osztalya, kezeli az eveneteket
+ */
 public class FieldControl {
     Player owner;
+    /**
+     * A fieldhez tartotó fieldview
+     */
     FIeldView fieldView;
+    /**
+     * A hozzatartozo field
+     */
     Field field;
 
+    /**
+     * Konstruktor, beallitja a hozza tartozo fieldet es fieldviewt
+     * @param fV a fieldview
+     * @param f a field
+     */
     public FieldControl(FIeldView fV, Field f) {
         field = f;
         fieldView = fV;
     }
 
+    /**
+     * Esemenykezelo, tovabblepteti a soron kovetkezo jatekost
+     */
     public void handleEvent() {
         Game.getInstance().gc.getTurnOf().move(field);
         Game.getInstance().gc.Update();
--- src/graphics/fields/LaboratoryView.java
+++ src/graphics/fields/LaboratoryView.java
@@ -4,8 +4,17 @@ import field.Field;
 
 import java.awt.*;
 
-public class LaboratoryView extends FIeldView {
+/**
+ * A laboratorium megjeleniteseert felelos osztaly
+ */
 
+public class LaboratoryView extends FIeldView {
+    /**
+     * Konstruktor, meghivja az os konsturktorat a megfeleo keppel
+     * @param r A megjelenites helye es meretete
+     * @param ImageName A kep ami az ikonhoz tartozik
+     * @param f a  field
+     */
     public LaboratoryView(Rectangle r, String ImageName, Field f) {
         super(r, "kepek/laboratory.png", f);
     }
